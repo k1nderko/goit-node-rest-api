7c0d737db210a05d00ca15a953ac4e42e9f9f447
k1nderko <107281239+k1nderko@users.noreply.github.com>
2024-06-08 01:33:41 +0300

    23

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
+// 
 mongoose.connect(DB_HOST)
 .then(() => {
   app.listen(PORT, () => {
